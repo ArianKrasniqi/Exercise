d2d58d6cff7ea092bd78b9313a5187d8631e120f
Arian Krasniqi <ariiankrasniqi@gmail.com>
2020-01-19 20:23:19 +0100

    DB Connect, SQL Insert Query

---
+++ connectDB.php
@@ -0,0 +1,29 @@
+<?php 
+
+$title = $_POST['title'];
+$description = $_POST['description'];
+
+$SERVERNAME = "localhost";
+$USERNAME = "root";
+$PASSWORD = "";
+$DB = "phpcourse";
+
+$conn = mysqli_connect($SERVERNAME, $USERNAME, $PASSWORD, $DB);
+
+// Check Connection
+if(!$conn){
+  die("Connection failed " . mysqli_connect_error());
+}
+
+echo "Connected";
+
+echo "<br>";
+
+$sqlQuery = "INSERT INTO testtable (title, description) VALUES (?,  ?)";
+
+if(mysqli_query($conn, $sqlQuery)){
+  echo "Data inserted";
+}else{
+  echo "Errorrrr";
+}
+?>
